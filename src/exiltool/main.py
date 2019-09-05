e3d8755cb97f805ba5067fd2e3bebd787686005d
Jean <jgiard@antidot.net>
2019-09-05 19:42:53 +0200

    Remove pyramid code

--- src/exiltool/main.py
+++ src/exiltool/main.py
@@ -1,13 +1,8 @@
 from flask import Flask
 from injector import Injector
-from pyramid.response import Response
 
 from exiltool.app import ExilApp
 from exiltool.module import ExilModule
-
-
-def hello_world(request):
-    return Response('Hello World!')
 
 
 def get_flask_app() -> Flask:
