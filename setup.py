f4ff59a17edd857548592cc99721770b7b8b0e54
Jean <jgiard@antidot.net>
2019-09-09 21:37:32 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-from distutils.core import setup
-
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(name='ExilTool',
       version='1.0',
       author='Jean Giard',
-      packages=find_packages(),
+      packages=find_packages(where='src'),
       package_dir={'': 'src'},
       install_requires=['injector', 'pymongo', 'pyckson', 'flask', 'flask-session']
       )
