d5807f78413ef60278ece6559c186689fb7a2009
Jean <jgiard@antidot.net>
2019-10-13 16:08:56 +0200

    Improve production formula

--- src/exiltool/map/tools.py
+++ src/exiltool/map/tools.py
@@ -1,6 +1,10 @@
 def compute_prod(land: int, native_prod: int):
-    mines = int((land - 20) / 2)
+    base_occupation = 9 + 2 + 2 + 6 + 6 + 2  # colony + construction + energy + storage + factories + naval yard
+    mines = int((land + 20 - base_occupation) / 2)
+    travs = 40000 - 9000 - mines * 1000  # base buildings + 10 sats
+    while travs < 0:
+        mines -= 1
+        travs += 6000
     prod = native_prod + mines * native_prod * 4
-    prod_eff = prod * 1.10
-    prod_total = prod * ((100 + mines) / 100)
-    return prod_total
+    prod_total = prod * ((100 + mines) / 100) * 1.14 * 1.10  # techs + buildings, efficency
+    return int(prod_total)
--- test/test_map_tools.py
+++ test/test_map_tools.py
@@ -2,4 +2,5 @@ from exiltool.map.tools import compute_prod
 
 
 def test_compute_prod():
-    assert compute_prod(80, 60) == 9438
+    assert compute_prod(80, 60) == 14321
+    assert compute_prod(120, 150) == 58226
